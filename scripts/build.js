cf84a3a92812f7d703d95ac3a721567c9f3126f9
Jason Garber <jason@sixtwothree.org>
2015-03-05 19:42:52 -0500

    Rewrite build script. Update bower.json and package.json.

---
+++ scripts/build.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+var childProcess = require('child_process'),
+	pkg = require('../package.json'),
+	preamble = '/*!\n' +
+		'*  RouterRouter ' + pkg.version + '\n' +
+		'*\n' +
+		'*  ' + pkg.description + '\n' +
+		'*\n' +
+		'*  Lovingly derived by ' + pkg.author.name + ' (' + pkg.author.url + ')\n' +
+		'*  Source code available at: ' + pkg.homepage + '\n' +
+		'*\n' +
+		'*  Backbone is\n' +
+		'*  (c) 2011-2014 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors\n' +
+		'*\n' +
+		'*  RouterRouter is\n' +
+		'*  (c) 2013—present ' + pkg.author.name + '\n' +
+		'*\n' +
+		'*  Like Backbone, RouterRouter may be freely distributed under the MIT license.\n' +
+		'*\n' +
+		'*  For more about Backbone, visit: http://backbonejs.org\n' +
+		'*/\n';
+
+childProcess.exec('uglifyjs src/routerrouter.js --beautify "indent-level=2" --preamble "' + preamble + '" --output dist/routerrouter.js');
+childProcess.exec('uglifyjs src/routerrouter.js --compress --mangle --preamble "' + preamble + '" --output dist/routerrouter.min.js');
